8dcedd747b360abbbdb2922e7fb87aaa69cdbc3e
Artem Shafranov <artem.shafranov@gmail.com>
2014-08-08 12:52:09 +0400

    Use random strings as entities numbers in demo project

--- NetLicensingClient-demo/src/main/java/com/labs64/netlicensing/demo/NetLicensingClientDemo.java
+++ NetLicensingClient-demo/src/main/java/com/labs64/netlicensing/demo/NetLicensingClientDemo.java
@@ -14,6 +14,7 @@ package com.labs64.netlicensing.demo;
 
 import java.math.BigDecimal;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
@@ -54,11 +55,7 @@ public class NetLicensingClientDemo {
     private final static int CODE_OK = 0;
     private final static int CODE_ERROR = 1;
 
-    private final static String DEMO_PRODUCT_NUMBER = "P001-DEMO";
-    private final static String DEMO_PRODUCT_MODULE_NUMBER = "PM001-DEMO";
-    private final static String DEMO_LICENSE_TEMPLATE_NUMBER = "LT001-DEMO";
-    private final static String DEMO_LICENSEE_NUMBER = "L001-DEMO";
-    private final static String DEMO_LICENSE_NUMBER = "LC001-DEMO";
+    private final static String DEMO_NUMBER_PREFIX = "DEMO-";
 
     public static void main(String[] args) {
 
@@ -71,6 +68,13 @@ public class NetLicensingClientDemo {
         context.setSecurityMode(SecurityMode.BASIC_AUTHENTICATION);
         context.setUsername("demo");
         context.setPassword("demo");
+
+        final String randomNumber = RandomStringUtils.randomAlphanumeric(8);
+        final String productNumber = numberWithPrefix("P", randomNumber);
+        final String productModuleNumber = numberWithPrefix("PM", randomNumber);
+        final String licenseTemplateNumber = numberWithPrefix("LT", randomNumber);
+        final String licenseeNumber = numberWithPrefix("L", randomNumber);
+        final String licenseNumber = numberWithPrefix("LC", randomNumber);
 
         final ConsoleWriter out = new ConsoleWriter();
 
@@ -90,12 +94,12 @@ public class NetLicensingClientDemo {
             // region ********* Product
 
             final Product newProduct = new ProductImpl();
-            newProduct.setNumber(DEMO_PRODUCT_NUMBER);
+            newProduct.setNumber(productNumber);
             newProduct.setName("Demo product");
             Product product = ProductService.create(context, newProduct);
             out.writeObject("Added product:", product);
 
-            product = ProductService.get(context, DEMO_PRODUCT_NUMBER);
+            product = ProductService.get(context, productNumber);
             out.writeObject("Got product:", product);
 
             Page<Product> products = ProductService.list(context);
@@ -103,10 +107,10 @@ public class NetLicensingClientDemo {
 
             final Product updateProduct = new ProductImpl();
             updateProduct.addProperty("Updated property name", "Updated value");
-            product = ProductService.update(context, DEMO_PRODUCT_NUMBER, updateProduct);
+            product = ProductService.update(context, productNumber, updateProduct);
             out.writeObject("Updated product:", product);
 
-            ProductService.delete(context, DEMO_PRODUCT_NUMBER, true);
+            ProductService.delete(context, productNumber, true);
             out.writeMessage("Deleted Product!");
 
             products = ProductService.list(context);
@@ -123,13 +127,13 @@ public class NetLicensingClientDemo {
             // region ********* ProductModule
 
             final ProductModule newProductModule = new ProductModuleImpl();
-            newProductModule.setNumber(DEMO_PRODUCT_MODULE_NUMBER);
+            newProductModule.setNumber(productModuleNumber);
             newProductModule.setName("Demo product module");
             newProductModule.setLicensingModel(Constants.LicensingModel.TimeLimitedEvaluation.NAME);
-            ProductModule productModule = ProductModuleService.create(context, DEMO_PRODUCT_NUMBER, newProductModule);
+            ProductModule productModule = ProductModuleService.create(context, productNumber, newProductModule);
             out.writeObject("Added product module:", productModule);
 
-            productModule = ProductModuleService.get(context, DEMO_PRODUCT_MODULE_NUMBER);
+            productModule = ProductModuleService.get(context, productModuleNumber);
             out.writeObject("Got product module:", productModule);
 
             Page<ProductModule> productModules = ProductModuleService.list(context);
@@ -137,16 +141,16 @@ public class NetLicensingClientDemo {
 
             final ProductModule updateProductModule = new ProductModuleImpl();
             updateProductModule.addProperty("Updated property name", "Updated property value");
-            productModule = ProductModuleService.update(context, DEMO_PRODUCT_MODULE_NUMBER, updateProductModule);
+            productModule = ProductModuleService.update(context, productModuleNumber, updateProductModule);
             out.writeObject("Updated product module:", productModule);
 
-            ProductModuleService.delete(context, DEMO_PRODUCT_MODULE_NUMBER, true);
+            ProductModuleService.delete(context, productModuleNumber, true);
             out.writeMessage("Deleted ProductModule!");
 
             productModules = ProductModuleService.list(context);
             out.writePage("Got the following ProductModules:", productModules);
 
-            productModule = ProductModuleService.create(context, DEMO_PRODUCT_NUMBER, newProductModule);
+            productModule = ProductModuleService.create(context, productNumber, newProductModule);
             out.writeObject("Added product module again:", productModule);
 
             productModules = ProductModuleService.list(context);
@@ -157,7 +161,7 @@ public class NetLicensingClientDemo {
             // region ********* LicenseTemplate
 
             final LicenseTemplate newLicenseTemplate = new LicenseTemplateImpl();
-            newLicenseTemplate.setNumber(DEMO_LICENSE_TEMPLATE_NUMBER);
+            newLicenseTemplate.setNumber(licenseTemplateNumber);
             newLicenseTemplate.setName("Demo Evaluation Period");
             newLicenseTemplate.setLicenseType(LicenseType.FEATURE);
             newLicenseTemplate.setPrice(new BigDecimal(12.5));
@@ -165,11 +169,11 @@ public class NetLicensingClientDemo {
             newLicenseTemplate.setAutomatic(false);
             newLicenseTemplate.setHidden(false);
             out.writeObject("Adding license template:", newLicenseTemplate);
-            LicenseTemplate licenseTemplate = LicenseTemplateService.create(context, DEMO_PRODUCT_MODULE_NUMBER,
+            LicenseTemplate licenseTemplate = LicenseTemplateService.create(context, productModuleNumber,
                     newLicenseTemplate);
             out.writeObject("Added license template:", licenseTemplate);
 
-            licenseTemplate = LicenseTemplateService.get(context, DEMO_LICENSE_TEMPLATE_NUMBER);
+            licenseTemplate = LicenseTemplateService.get(context, licenseTemplateNumber);
             out.writeObject("Got licenseTemplate:", licenseTemplate);
 
             Page<LicenseTemplate> licenseTemplates = LicenseTemplateService.list(context);
@@ -177,17 +181,17 @@ public class NetLicensingClientDemo {
 
             final LicenseTemplate updateLicenseTemplate = new LicenseTemplateImpl();
             updateLicenseTemplate.addProperty("Updated property name", "Updated value");
-            licenseTemplate = LicenseTemplateService.update(context, DEMO_LICENSE_TEMPLATE_NUMBER,
+            licenseTemplate = LicenseTemplateService.update(context, licenseTemplateNumber,
                     updateLicenseTemplate);
             out.writeObject("Updated license template:", licenseTemplate);
 
-            LicenseTemplateService.delete(context, DEMO_LICENSE_TEMPLATE_NUMBER, true);
+            LicenseTemplateService.delete(context, licenseTemplateNumber, true);
             out.writeMessage("Deleted LicenseTemplate!");
 
             licenseTemplates = LicenseTemplateService.list(context);
             out.writePage("Got the following license templates:", licenseTemplates);
 
-            licenseTemplate = LicenseTemplateService.create(context, DEMO_PRODUCT_MODULE_NUMBER, newLicenseTemplate);
+            licenseTemplate = LicenseTemplateService.create(context, productModuleNumber, newLicenseTemplate);
             out.writeObject("Added license template again:", licenseTemplate);
 
             licenseTemplates = LicenseTemplateService.list(context);
@@ -198,28 +202,28 @@ public class NetLicensingClientDemo {
             // region ********* Licensee
 
             final Licensee newLicensee = new LicenseeImpl();
-            newLicensee.setNumber(DEMO_LICENSEE_NUMBER);
-            Licensee licensee = LicenseeService.create(context, DEMO_PRODUCT_NUMBER, newLicensee);
+            newLicensee.setNumber(licenseeNumber);
+            Licensee licensee = LicenseeService.create(context, productNumber, newLicensee);
             out.writeObject("Added licensee:", licensee);
 
             Page<Licensee> licensees = LicenseeService.list(context);
             out.writePage("Got the following licensees:", licensees);
 
-            LicenseeService.delete(context, DEMO_LICENSEE_NUMBER, true);
+            LicenseeService.delete(context, licenseeNumber, true);
             out.writeMessage("Deleted licensee!");
 
             licensees = LicenseeService.list(context);
             out.writePage("Got the following licensees after delete:", licensees);
 
-            licensee = LicenseeService.create(context, DEMO_PRODUCT_NUMBER, newLicensee);
+            licensee = LicenseeService.create(context, productNumber, newLicensee);
             out.writeObject("Added licensee again:", licensee);
 
-            licensee = LicenseeService.get(context, DEMO_LICENSEE_NUMBER);
+            licensee = LicenseeService.get(context, licenseeNumber);
             out.writeObject("Got licensee:", licensee);
 
             final Licensee updateLicensee = new LicenseeImpl();
             updateLicensee.addProperty("Updated property name", "Updated value");
-            licensee = LicenseeService.update(context, DEMO_LICENSEE_NUMBER, updateLicensee);
+            licensee = LicenseeService.update(context, licenseeNumber, updateLicensee);
             out.writeObject("Updated licensee:", licensee);
 
             licensees = LicenseeService.list(context);
@@ -230,30 +234,30 @@ public class NetLicensingClientDemo {
             // region ********* License
 
             final License newLicense = new LicenseImpl();
-            newLicense.setNumber(DEMO_LICENSE_NUMBER);
-            License license = LicenseService.create(context, DEMO_LICENSEE_NUMBER, DEMO_LICENSE_TEMPLATE_NUMBER, null,
+            newLicense.setNumber(licenseNumber);
+            License license = LicenseService.create(context, licenseeNumber, licenseTemplateNumber, null,
                     newLicense);
             out.writeObject("Added license:", license);
 
             Page<License> licenses = LicenseService.list(context);
             out.writePage("Got the following license templates:", licenses);
 
-            LicenseService.delete(context, DEMO_LICENSE_NUMBER, true);
+            LicenseService.delete(context, licenseNumber, true);
             out.writeMessage("Deleted license!");
 
             licenses = LicenseService.list(context);
             out.writePage("Got the following license templates:", licenses);
 
-            license = LicenseService.create(context, DEMO_LICENSEE_NUMBER, DEMO_LICENSE_TEMPLATE_NUMBER, null,
+            license = LicenseService.create(context, licenseeNumber, licenseTemplateNumber, null,
                     newLicense);
             out.writeObject("Added license again:", license);
 
-            license = LicenseService.get(context, DEMO_LICENSE_NUMBER);
+            license = LicenseService.get(context, licenseNumber);
             out.writeObject("Got license:", license);
 
             final License updateLicense = new LicenseImpl();
             updateLicense.addProperty("Updated property name", "Updated value");
-            license = LicenseService.update(context, DEMO_LICENSE_NUMBER, null, updateLicense);
+            license = LicenseService.update(context, licenseNumber, null, updateLicense);
             out.writeObject("Updated license:", license);
 
             // endregion
@@ -274,7 +278,7 @@ public class NetLicensingClientDemo {
 
             context.setApiKey(apiKey.getNumber());
             newToken.setTokenType(TokenType.SHOP);
-            newToken.addProperty(Constants.Licensee.LICENSEE_NUMBER, DEMO_LICENSEE_NUMBER);
+            newToken.addProperty(Constants.Licensee.LICENSEE_NUMBER, licenseeNumber);
             context.setSecurityMode(SecurityMode.APIKEY_IDENTIFICATION);
             final Token shopToken = TokenService.create(context, newToken);
             context.setSecurityMode(SecurityMode.BASIC_AUTHENTICATION);
@@ -293,12 +297,12 @@ public class NetLicensingClientDemo {
 
             // region ********* Validate
 
-            ValidationResult validationResult = LicenseeService.validate(context, DEMO_LICENSEE_NUMBER,
-                    DEMO_PRODUCT_NUMBER, null);
+            ValidationResult validationResult = LicenseeService.validate(context, licenseeNumber,
+                    productNumber, null);
             out.writeObject("Validation result for created licensee:", validationResult);
 
             context.setSecurityMode(SecurityMode.APIKEY_IDENTIFICATION);
-            validationResult = LicenseeService.validate(context, DEMO_LICENSEE_NUMBER, DEMO_PRODUCT_NUMBER, null);
+            validationResult = LicenseeService.validate(context, licenseeNumber, productNumber, null);
             context.setSecurityMode(SecurityMode.BASIC_AUTHENTICATION);
             out.writeObject("Validation repeated with API Key:", validationResult);
 
@@ -323,7 +327,7 @@ public class NetLicensingClientDemo {
                 }
 
                 // delete test product with all its related items
-                ProductService.delete(context, DEMO_PRODUCT_NUMBER, true);
+                ProductService.delete(context, productNumber, true);
 
             } catch (BaseCheckedException e) {
                 out.writeException("Got NetLicensing exception during cleanup:", e);
@@ -335,6 +339,10 @@ public class NetLicensingClientDemo {
                 System.exit(exitCode);
             }
         }
+    }
+
+    private static String numberWithPrefix(final String prefix, String number) {
+        return String.format("%s%s%s", DEMO_NUMBER_PREFIX, prefix, number);
     }
 
 }
