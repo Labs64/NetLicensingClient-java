29ca22a014646068c4d24b7af15682e72bc8ac7c
R.Brown <ready.brown@hotmail.de>
2016-09-11 08:02:15 +0200

    - limit available payment methods set

--- NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/PaymentMethodEnum.java
+++ NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/PaymentMethodEnum.java
@@ -15,7 +15,7 @@ package com.labs64.netlicensing.domain.vo;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- * This enum defines available payment methods.
+ * This enum defines payment methods supported by NetLicensing.
  */
 public enum PaymentMethodEnum {
 
@@ -24,25 +24,13 @@ public enum PaymentMethodEnum {
      */
     NULL,
 
-    DIRECT_DEBIT,
-
     PAYPAL,
 
     PAYPAL_SANDBOX,
 
     STRIPE,
 
-    STRIPE_TESTING,
-
-    CREDIT_CARD,
-
-    GOOGLE_CHECKOUT,
-
-    CASH_ON_DELIVERY,
-
-    PAYMENT_ON_INVOICE,
-
-    FINANCING;
+    STRIPE_TESTING;
 
     public static PaymentMethodEnum parseString(final String paymentMethod) {
         if (StringUtils.isBlank(paymentMethod)) {
