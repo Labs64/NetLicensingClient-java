8245c70526fd6385e3a20e9b64b0a80076cc402b
Alexey Averikhin <alexey.averikhin@labs64.de>
2021-12-20 16:58:56 +0100

    Update vulnerability at xmldsig to v4.4.2 + plugin / dependencies update

--- NetLicensingClient/src/main/java/com/labs64/netlicensing/util/XMLDSigValidatorCustom.java
+++ NetLicensingClient/src/main/java/com/labs64/netlicensing/util/XMLDSigValidatorCustom.java
@@ -16,8 +16,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import com.helger.commons.ValueEnforcer;
-import com.helger.commons.collection.ext.CommonsArrayList;
-import com.helger.commons.collection.ext.ICommonsList;
+import com.helger.commons.collection.impl.CommonsArrayList;
+import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.xmldsig.XMLDSigSetup;
 import com.helger.xmldsig.XMLDSigValidationResult;
 
