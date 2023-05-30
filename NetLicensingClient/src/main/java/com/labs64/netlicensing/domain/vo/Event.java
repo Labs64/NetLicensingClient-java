0428ba35c7ab0c6f879deab7bc03ec455782647b
Slava Rudkovskyi <xraskolx@gmail.com>
2023-05-30 18:11:55 +0200

    add warningLevel events

--- NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/Event.java
+++ NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/Event.java
@@ -3,7 +3,12 @@ package com.labs64.netlicensing.domain.vo;
 import java.util.Arrays;
 
 public enum Event {
-    CREATE_LICENSEE, CREATE_LICENSE;
+    CREATE_LICENSEE, 
+    CREATE_LICENSE,
+    WARNING_LEVEL_TO_ANY,
+    WARNING_LEVEL_TO_GREEN,
+    WARNING_LEVEL_TO_YELLOW,
+    WARNING_LEVEL_TO_RED;;
 
     public static Event parseString(final String value) {
         return Arrays.stream(Event.values()).filter((e) -> e.name().equalsIgnoreCase(value)).findFirst().orElse(null);
