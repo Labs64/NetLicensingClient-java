bbae7717ffe15bc15a44db493268106462f120eb
Viacheslav Rudkovskyi <viachaslau.rudkovski@labs64.de>
2023-12-05 19:03:28 +0200

    rename events

--- NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/Event.java
+++ NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/Event.java
@@ -3,12 +3,9 @@ package com.labs64.netlicensing.domain.vo;
 import java.util.Arrays;
 
 public enum Event {
-    CREATE_LICENSEE, 
-    CREATE_LICENSE,
-    WARNING_LEVEL_TO_ANY,
-    WARNING_LEVEL_TO_GREEN,
-    WARNING_LEVEL_TO_YELLOW,
-    WARNING_LEVEL_TO_RED;
+    LICENSEE_CREATED,
+    LICENSE_CREATED,
+    WARNING_LEVEL_CHANGED;
 
     public static Event parseString(final String value) {
         return Arrays.stream(Event.values()).filter((e) -> e.name().equalsIgnoreCase(value)).findFirst().orElse(null);
