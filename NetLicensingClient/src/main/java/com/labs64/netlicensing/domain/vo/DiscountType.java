0ac33ed8e9f9bce22a22352f45affd7cbf2cb8a0
Viacheslav Rudkovskyi <xraskolx@gmail.com>
2025-05-16 18:03:45 +0300

    add DiscountType

---
+++ NetLicensingClient/src/main/java/com/labs64/netlicensing/domain/vo/DiscountType.java
@@ -0,0 +1,18 @@
+package com.labs64.netlicensing.domain.vo;
+
+public enum DiscountType {
+    FIX,
+
+    PERCENT;
+
+    public static DiscountType parseString(final String discountType) {
+        if (discountType != null) {
+            for (final DiscountType type : DiscountType.values()) {
+                if (discountType.equalsIgnoreCase(type.name())) {
+                    return type;
+                }
+            }
+        }
+        return null;
+    }
+}
